1c4708349f6eef36aabd503677ad8c2a366a5928
Rakoski <mastrakoski@gmail.com>
2023-10-02 08:10:38 -0300

    feat: Criado serviço de venda e produtos
    Esse serviço será utilizado para o cadastro de vendas com seus respectivos produtos

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-// Create a Sequelize instance with your database configuration
 const sequelize = new Sequelize("apice", "Mateus", "Mateus0312", {
-    dialect: "mysql", // Replace with your database dialect
-    host: "localhost", // Replace with your database host
+    dialect: "mysql",
+    host: "localhost",
+    database: "apice", // Add this line to specify the database
 });
 
 module.exports = sequelize;
